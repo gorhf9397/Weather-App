b8b86d11d615490e8b707673768db6d64c71c3cb
gorhf9397 <trr456@naver.com>
2023-02-07 21:11:50 +0900

    expo i expo-Locassion 설치expoLocation사용지리좌표,위치구역명호출

--- App.js
+++ App.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
 // expo statusBar 와 reactNative statusBar 가 따로 있는 이유
 // expo 가 reactNative api들을 복사하고 개선하기를 결정 했기 때문
+import * as Location from "expo-location";
 
 // Dimentions 화면 크기를 알려주는 API https://reactnative.dev/docs/dimensions
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 // console.log(SCREEN_WIDTH);
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [location, setLocation] = useState(null);
+  const [ok, setOk] = useState(true);
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    // console.log(permission);
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      // 유저의 지리 좌표
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      // 유저의 위치 구역명
+      {
+        latitude,
+        longitude,
+      },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+  };
+  // 위치 정보 expoLocation
+  // https://docs.expo.dev/versions/v47.0.0/sdk/location/#locationrequestpermissionsasync
+  useEffect(() => {
+    ask();
+  }, []);
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       {/* reactNative는 components 이기 때문에 스크롤 을 사용하려면ScrollView 사용
       https://reactnative.dev/docs/scrollview horizontal 좌우 스크롤 
