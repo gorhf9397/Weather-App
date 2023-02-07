692038436246fb712757a3f958e71e8e56f87dee
gorhf9397 <trr456@naver.com>
2023-02-07 20:19:05 +0900

    style 내용정리 사용 API 에 대한 주석 링크 추가

--- App.js
+++ App.js
@@ -1,27 +1,76 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native"
+import React from "react";
+import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
 // expo statusBar 와 reactNative statusBar 가 따로 있는 이유
-// expo 가 reactNative api들을 복사하고 개선하기를 결정 했기 때문  
+// expo 가 reactNative api들을 복사하고 개선하기를 결정 했기 때문
+
+// Dimentions 화면 크기를 알려주는 API https://reactnative.dev/docs/dimensions
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+// console.log(SCREEN_WIDTH);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>안녕하세요</Text>
-      <StatusBar style="auto" />
-      {/* 스마트폰 상단바에 해당하는 StatusBar */}
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      {/* reactNative는 components 이기 때문에 스크롤 을 사용하려면ScrollView 사용
+      https://reactnative.dev/docs/scrollview horizontal 좌우 스크롤 
+      pagingEnabled 스크롤 제한하며 페이지 생성 https://reactnative.dev/docs/scrollview#pagingenabled 
+      showsHorizontalScrollIndicator 하단 스크롤바 숨김 https://reactnative.dev/docs/scrollview#showshorizontalscrollindicator*/}
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // style이 object 형태 StyleSheet.create 형태를 사용하면 자동완성기능 으로 편하게 사용가능
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "orange",
   },
-  text: {
-    fontSize: 28,
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 58,
+    fontWeight: "500",
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 168,
+  },
+  description: {
+    fontSize: 60,
   },
 });
