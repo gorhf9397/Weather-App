989c76aca722152830227ef19c1492dbeba3785a
gorhf9397 <trr456@naver.com>
2023-02-08 00:39:55 +0900

    expo/vector-icons 사용 하여 스타일 es6 문법으로 저장하여 한번에 뿌려주기

--- App.js
+++ App.js
@@ -10,11 +10,24 @@ import {
 // expo statusBar 와 reactNative statusBar 가 따로 있는 이유
 // expo 가 reactNative api들을 복사하고 개선하기를 결정 했기 때문
 import * as Location from "expo-location";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Fontisto } from "@expo/vector-icons";
 
 // Dimentions 화면 크기를 알려주는 API https://reactnative.dev/docs/dimensions
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 // console.log(SCREEN_WIDTH);
 const API_KEY = "5044bd9e7f09bce577e61eea0811b2a4";
+
+const icons = {
+  Clear: "day-sunny",
+  Clouds: "cloudy",
+  Rain: "rain",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Drizzle: "day-rain",
+  Tuunderstorm: "lightning",
+};
+
 export default function App() {
   const [city, setCity] = useState("Loading...");
   const [days, setDays] = useState([]);
@@ -30,6 +43,7 @@ export default function App() {
       coords: { latitude, longitude },
     } = await Location.getCurrentPositionAsync({ accuracy: 5 });
     const location = await Location.reverseGeocodeAsync(
+      // 위도와 경도로 주소를 알려줌
       // 유저의 위치 구역명
       {
         latitude,
@@ -44,6 +58,7 @@ export default function App() {
     const json = await response.json();
     setDays(json.daily);
   };
+  const today = new Date();
   // 위치 정보 expoLocation
   // https://docs.expo.dev/versions/v47.0.0/sdk/location/#locationrequestpermissionsasync
   useEffect(() => {
@@ -58,6 +73,9 @@ export default function App() {
       https://reactnative.dev/docs/scrollview horizontal 좌우 스크롤 
       pagingEnabled 스크롤 제한하며 페이지 생성 https://reactnative.dev/docs/scrollview#pagingenabled 
       showsHorizontalScrollIndicator 하단 스크롤바 숨김 https://reactnative.dev/docs/scrollview#showshorizontalscrollindicator*/}
+      <Text style={styles.month}>
+        {today.getDay() + " / " + today.getDate()}
+      </Text>
       <ScrollView
         pagingEnabled
         horizontal
@@ -71,10 +89,24 @@ export default function App() {
         ) : (
           days.map((day, index) => (
             <View key={index} style={styles.day}>
-              <Text style={styles.temp}>
-                {parseFloat(day.temp.day).toFixed(1)}
-                <Text style={styles.temperature}>°C</Text>
-              </Text>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <View style={{ flexDirection: "column", alignItems: "center" }}>
+                  <Fontisto
+                    name={icons[day.weather[0].main]}
+                    size={50}
+                    color="black"
+                  />
+                  <MaterialCommunityIcons
+                    name="temperature-celsius"
+                    size={50}
+                    color="black"
+                    style={{marginTop: 40}}
+                  />
+                </View>
+              </View>
               <Text style={styles.description}>{day.weather[0].main}</Text>
               <Text style={styles.tinyText}>{day.weather[0].description}</Text>
             </View>
@@ -94,6 +126,8 @@ const styles = StyleSheet.create({
     flex: 1.2,
     justifyContent: "center",
     alignItems: "center",
+    borderBottomWidth: 5,
+    borderStyle: "dashed",
   },
   cityName: {
     fontSize: 58,
@@ -113,7 +147,9 @@ const styles = StyleSheet.create({
   tinyText: {
     fontSize: 20,
   },
-  temperature: {
-    fontSize: 40,
+  month: {
+    fontSize: 30,
+    textAlign: "center",
+    marginTop: 30,
   },
 });
